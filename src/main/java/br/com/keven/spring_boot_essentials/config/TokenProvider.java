fade84b501b7dd32f79b49b885f261f6dfec5c3d
kevenn <kevengodinho3474@gmail.com>
2026-05-11 21:29:00 -0300

    Token Provider

--- src/main/java/br/com/keven/spring_boot_essentials/config/TokenProvider.java
+++ src/main/java/br/com/keven/spring_boot_essentials/config/TokenProvider.java
@@ -1,4 +1,68 @@
 package br.com.keven.spring_boot_essentials.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Component
 public class TokenProvider {
+
+    @Value("${jwt.expiration}")
+    private long expirationTime;
+
+    @Value("${jwt.key}")
+    private String key;
+
+    //Gerar um token
+    public String gerarToken(Authentication authentication){
+        UserDetails user = (UserDetails) authentication.getPrincipal();
+        return buildToken(user.getUsername());
+    }
+
+    private String buildToken(String username){
+        Date now = new Date();
+        Date expiration = new Date(now.getTime() + expirationTime);
+
+        return Jwts.builder()
+                .subject(username)
+                .issuedAt(now)
+                .expiration(expiration)
+                .signWith(getSigningKey())
+                .compact();
+    }
+    private SecretKey getSigningKey(){
+        return Keys.hmacShaKeyFor(key.getBytes());
+    }
+
+    //Validar um token
+
+    public boolean isTokenValid(String token){
+        try{
+            getClaims(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+    private Claims getClaims(String token){
+        //validar assinatura
+        //validar a expiração do token
+        return  Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    //extrair inf do token
+    private String getUsername(String token){
+        return getClaims(token).getSubject();
+    }
 }
